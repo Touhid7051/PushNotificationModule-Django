ec04604a84416012bb2ac187fa0791d6a00c054d
Touhid <touhidul15-7051@diu.edu.bd>
2021-01-20 16:08:30 +0600

    Notification Count

--- Notification/views.py
+++ Notification/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Notification
 
 # Create your views here.
 
 def index(request):
-    return render(request,'dashboard/index.html')
+    if not request.user.is_authenticated:
+        return render(request,'dashboard/notAuth.html')     
+    unread_notification_count = Notification.objects.filter(assigned_to=request.user,is_read=False).count()
+    context = {
+        'unread_notification_count':unread_notification_count
+    }
+    return render(request,'dashboard/index.html', context=context)
     
     
