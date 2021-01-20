6edb7747eee16650fccf1ca7c4fa2a3b5c1e3f43
Touhid <touhidul15-7051@diu.edu.bd>
2021-01-20 18:10:24 +0600

    Showing old/read notification in card

--- Notification/views.py
+++ Notification/views.py
@@ -5,11 +5,14 @@ from .models import Notification
 
 def index(request):
     if not request.user.is_authenticated:
-        return render(request,'dashboard/notAuth.html')     
-    unread_notifications = request.user.notifications_assigned_to_user.filter(is_read=False).order_by('-creation_date')
+        return render(request,'dashboard/notAuth.html')
+    notifications = request.user.notifications_assigned_to_user.order_by('-creation_date')
+    old_notifications = notifications.filter(is_read=True)   
+    unread_notifications = request.user.notifications_assigned_to_user.filter(is_read=False).order_by('creation_date')
     context = {
         'unread_notification_count':unread_notifications.count(),
-        'unread_notifications':unread_notifications[:5]
+        'unread_notifications':unread_notifications[:5],
+        'old_notifications':old_notifications[:3]
     }
     return render(request,'dashboard/index.html', context=context)
     
